8b45429341acd27b93e6f1c3407ee4f0253726d9
student <krzysztof.konwisarz@gmail.com>
2015-06-09 21:10:35 +0200

    Kontekst aplikacji oraz aplikacja częściowo przeniesiona na Springa
    applicationContext.xml do pobrania w trakcie zajęć: https://gist.github.com/krzysztofkonwisarz/6d4bd1bd32a6dd45ce79
    
    Cel: pokazanie różnicy między klasycznym tworzeniem instancji, a otrzymywaniem jej z kontenera IoC
    
    Zadanie: Uczynić jedną z klas (środkową w ciągu wywołań) springowym beanem

--- src/main/java/pl/edu/uksw/j2eecourse/zajecia_2/Aplikacja.java
+++ src/main/java/pl/edu/uksw/j2eecourse/zajecia_2/Aplikacja.java
@@ -1,9 +1,15 @@
 package pl.edu.uksw.j2eecourse.zajecia_2;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Aplikacja {
 
     public static void main(String[] args) {
-        Swiat swiat = new Swiat();
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+        Swiat swiat = applicationContext.getBean(Swiat.class);
         swiat.start();
     }
 
